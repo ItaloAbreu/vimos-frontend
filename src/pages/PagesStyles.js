cb8965380d5144587b4bc34f165a7e4328d03cf2
Italo Abreu <italodw@gmail.com>
2019-07-10 19:51:00 -0300

    Posição dos Cards

--- src/pages/PagesStyles.js
+++ src/pages/PagesStyles.js
@@ -2,8 +2,8 @@ import styled from 'styled-components';
 
 export const Content = styled.section`
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
-  align-items: center;
 
   min-height: calc(100vh - 60px);
 `;
