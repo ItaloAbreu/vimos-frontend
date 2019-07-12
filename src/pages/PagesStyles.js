cfbbe44759bad7cfaf0efeb0e65de1ad7340ab5a
Italo Abreu <italodw@gmail.com>
2019-07-12 19:17:29 -0300

    Upload estilizado.

--- src/pages/PagesStyles.js
+++ src/pages/PagesStyles.js
@@ -8,3 +8,125 @@ export const Content = styled.section`
 
   min-height: calc(100vh - 60px);
 `;
+
+export const FormStyled = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  background-color: #252525;
+  color: #999;
+  border-radius: 10px;
+  border: solid 1px #050505;
+
+  box-shadow: 0 0 10px #000d;
+  
+  min-width: 400px;
+  min-height: 440px;
+  max-width: 400px;
+  max-height: 440px;
+
+  overflow: hidden;
+
+  section {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    background-color: #111;
+    color: #fff;
+
+    width: 400px;
+    height: 45px;
+  }
+  
+
+  input[type="file"] {
+    width: 360px;
+    padding: 10px 0;
+    margin: 0;
+    border-bottom: solid 1px #151515;
+
+    ::-webkit-file-upload-button {
+      background-color: #333;
+      color: #fff;
+      padding: 10px;
+
+      border-radius: 7px;
+      border: solid 1px #151515;
+      border-bottom-width: 2px;
+
+      :hover {
+        background-color: #373737;
+      }
+
+      :active {
+        background-color: #151515;
+      }
+    }
+  }
+
+  label.info {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    font-size: 14px;
+
+    width: 360px;
+    margin: 10px 0;
+
+    input, textarea {
+      background-color: #333;
+      color: #fff;
+
+      padding: 10px;
+      border-radius: 5px;
+      border: solid 1px #151515;
+    }
+
+    input, textarea {
+      width: 290px;
+    }
+
+    textarea {
+      resize: none;
+      height: 140px;
+    }
+  }
+
+  label.agree {
+    display: flex;
+    font-size: 14px;
+    margin: 10px;
+    align-items: center;
+
+    input {
+      margin-right: 10px;
+    }
+  }
+
+  button {
+    background: #ac68ac;
+    color: #fff;
+
+    width: 280px;
+    height: 50px;
+    margin: 10px;
+
+    font-size: 18px;
+    font-weight: 400;
+
+    border-radius: 7px;
+    border: solid 1px #151515;
+    border-bottom-width: 2px;
+
+    :hover {
+      background: #ba82ba;
+    }
+
+    :active {
+      background: #381f38;
+    }
+  }
+`;
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,13 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
-import { Content } from './PagesStyles';
+import { Content, FormStyled } from './PagesStyles';
 
-function Upload() {
-  return (
-    <Content>
-      <h1>Upload!</h1>
-    </Content>
-  );
+class Upload extends Component {
+
+  submitForm = async (event) => {
+    event.preventDefault();
+  
+    const form = event.target;
+    const data = new FormData(form);
+  
+    const config = {
+      method: 'post',
+      body: data,
+    };
+  
+    return fetch('http://localhost:9091/video/storage', config)
+      .catch(err => console.error(err));
+  };
+
+  render() {
+    return (
+      <Content>
+        <FormStyled name="uploadForm" onSubmit={this.submitForm}
+        action='file/upload' method='post'
+        encType="multipart/form-data">
+          <section>Upload do Vídeo</section>
+          <input type="file" name="video"/>
+          <label className="info">Título
+            <input type="text" name="title"
+            placeholder="Título do vídeo."
+            /></label>
+          <label className="info">Descrição
+            <textarea type="text" name="description"
+            placeholder="Uma descrição legal."
+            maxLength="600"
+            /></label>
+          <label className="agree">
+            <input type="checkbox" name="agree"/>
+            <p>
+              Afirmo que li e concordo com
+              os <NavLink to="/license" target="new">
+              termos de uso</NavLink>.
+            </p>
+          </label>
+          <button type='submit'>Enviar!</button>
+        </FormStyled>
+      </Content>
+    );
+  };
 }
 
 export default Upload;
