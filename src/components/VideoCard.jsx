fc0a56441f5e2c02e355ff80b592eab1eebffdf2
Italo Abreu <italodw@gmail.com>
2019-07-19 02:04:14 -0300

    Subtítulo de distancia da data de criação.

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -20,14 +20,60 @@ export default class VideoCard extends Component {
 	}
 
 	mountDate(createdAt) {
+		// createdAt tem esse formato: "2018-07-17 20:05:02.138Z"
+		const currentDate = new Date();
 		const months = [
 			'Janeiro', 'Fevereiro', 'Março', 'Abril',
 			'Maio', 'Junho', 'Julho', 'Agosto',
 			'Setembro', 'Outubro', 'Novembro', 'Dezembro'
 		];
-		let date = createdAt.slice(0, 10).split('-');
-		date[1] = months[parseInt(date[1]) - 1];
-		return `Upload em ${date[2]} de ${date[1]} de ${date[0]}`;
+
+		const numbers = [
+			'um', 'dois', 'três', 'quatro', 'cinco',
+			'seis', 'sete', 'oito', 'nove', 'dez',
+			'onze', 'doze', 'treze', 'quatorze', 'quinze',
+			'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte',
+			'vinte e um', 'vinte e dois', 'vinte e três', 'vinte e quatro', 'vinte e cinco',
+			'vinte e seis', 'vinte e sete', 'vinte e oito', 'vinte e nove', 'trinta'
+		];
+
+		const sliceDate = createdAt.slice(0, 10).split('-')
+			.map(str => parseInt(str));
+		const sliceTime = createdAt.slice(11, 19).split(':')
+			.map(str => parseInt(str));
+
+		const date = {
+			year: sliceDate[0],
+			month: sliceDate[1] - 1,
+			day: sliceDate[2]
+		};
+		const time = {
+			hour: sliceTime[0],
+			minute: sliceTime[1],
+			second: sliceTime[2]
+		};
+		
+		
+		if (date.year < currentDate.getFullYear()) {
+			return `Em ${months[date.month]} de ${date.year}`;
+		} else if (date.month < currentDate.getMonth()) {
+			const monthsAgo = currentDate.getMonth() - (date.month + 1);
+			return `Há ${numbers[monthsAgo]} ${monthsAgo > 0? 'meses': 'mes'}`;
+		} else if (date.day < currentDate.getDate()) {
+			const daysAgo = (currentDate.getDate() - date.day);
+			return `Há ${numbers[daysAgo]} ${daysAgo > 0? 'dias': 'dia'}`;
+		} else if (time.hour < currentDate.getHours()) {
+			const hoursAgo = (currentDate.getHours() - time.hour);
+			return `${hoursAgo} ${hoursAgo > 1? 'horas': 'hora'} atrás`;
+		} else if (time.minute < currentDate.getMinutes()) {
+			const minutesAgo = (currentDate.getMinutes() - time.minute);
+			return `${minutesAgo} ${minutesAgo > 1? 'minutos': 'minuto'} atrás`;
+		}  else if (time.second < currentDate.getSeconds()) {
+			const secondsAgo = (currentDate.getSeconds() - time.second);
+			return `${secondsAgo} ${secondsAgo > 1? 'segundos': 'segundo'} atrás`;
+		} else {
+			return 'Agora mesmo';
+		}
 	}
 
 	mountTitle(title) {
@@ -49,4 +95,4 @@ export default class VideoCard extends Component {
 			</VideoCardStyled>
 		);
 	}
-}
+};
