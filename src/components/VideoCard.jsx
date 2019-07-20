a80c84aae93e241c2c9e1a9887b2147657995b46
Italo Abreu <italodw@gmail.com>
2019-07-20 02:41:35 -0300

    Tempo corrigido.

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,6 +4,19 @@ import { baseURL } from '../services/api';
 
 import { VideoCardStyled } from './ComponentsStyles';
 
+
+const months = [
+	'Janeiro', 'Fevereiro', 'Março', 'Abril',
+	'Maio', 'Junho', 'Julho', 'Agosto',
+	'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+];
+
+const numbers = [
+	'um', 'dois', 'três', 'quatro', 'cinco',
+	'seis', 'sete', 'oito', 'nove', 'dez',
+	'onze', 'doze'
+];
+
 export default class VideoCard extends Component {
 	state = {
 		videoinfo: {},
@@ -19,58 +32,64 @@ export default class VideoCard extends Component {
 		}});
 	}
 
+	diffOfTimes(previousDate, currentDate, exit='days') {
+		const timeDiff = Math.abs(currentDate.getTime() - previousDate.getTime());
+		let diffExit;
+
+		if (exit === 'days') {
+			diffExit = Math.ceil(timeDiff / (1000 * 3600 * 24) - 1); 
+		} else if (exit === 'hours') {
+			diffExit = Math.floor((timeDiff % 86400000) / 3600000); 
+		} else if (exit === 'minutes') {
+			diffExit = Math.round(((timeDiff % 86400000) % 3600000) / 60000); 
+		} else if (exit === 'seconds') {
+			diffExit = Math.round((((timeDiff % 86400000) % 3600000) % 60000) / 600); 
+		}
+		return diffExit;
+	}
+
 	mountDate(createdAt) {
 		// createdAt tem esse formato: "2018-07-17 20:05:02.138Z"
+		const previousDate = new Date(createdAt);
 		const currentDate = new Date();
-		const months = [
-			'Janeiro', 'Fevereiro', 'Março', 'Abril',
-			'Maio', 'Junho', 'Julho', 'Agosto',
-			'Setembro', 'Outubro', 'Novembro', 'Dezembro'
-		];
 
-		const numbers = [
-			'um', 'dois', 'três', 'quatro', 'cinco',
-			'seis', 'sete', 'oito', 'nove', 'dez',
-			'onze', 'doze', 'treze', 'quatorze', 'quinze',
-			'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte',
-			'vinte e um', 'vinte e dois', 'vinte e três', 'vinte e quatro', 'vinte e cinco',
-			'vinte e seis', 'vinte e sete', 'vinte e oito', 'vinte e nove', 'trinta'
-		];
+		const diffDays = this.diffOfTimes(previousDate, currentDate);
+		const diffHours = this.diffOfTimes(previousDate, currentDate, 'hours');
+		const diffMinutes = this.diffOfTimes(previousDate, currentDate, 'minutes');
+		const diffSeconds = this.diffOfTimes(previousDate, currentDate, 'seconds');
 
-		const sliceDate = createdAt.slice(0, 10).split('-')
-			.map(str => parseInt(str));
-		const sliceTime = createdAt.slice(11, 19).split(':')
-			.map(str => parseInt(str));
+		if (diffDays >= 364) {
+			// quando o post tem um ano ou mais
+			return `Em ${months[previousDate.getMonth()]} de ${previousDate.getFullYear()}`;
 
-		const date = {
-			year: sliceDate[0],
-			month: sliceDate[1] - 1,
-			day: sliceDate[2]
-		};
-		const time = {
-			hour: sliceTime[0],
-			minute: sliceTime[1],
-			second: sliceTime[2]
-		};
-		
-		
-		if (date.year < currentDate.getFullYear()) {
-			return `Em ${months[date.month]} de ${date.year}`;
-		} else if (date.month < currentDate.getMonth()) {
-			const monthsAgo = currentDate.getMonth() - (date.month + 1);
-			return `Há ${numbers[monthsAgo]} ${monthsAgo > 0? 'meses': 'mes'}`;
-		} else if (date.day < currentDate.getDate()) {
-			const daysAgo = (currentDate.getDate() - date.day);
-			return `Há ${numbers[daysAgo]} ${daysAgo > 0? 'dias': 'dia'}`;
-		} else if (time.hour < currentDate.getHours()) {
-			const hoursAgo = (currentDate.getHours() - time.hour);
-			return `${hoursAgo} ${hoursAgo > 1? 'horas': 'hora'} atrás`;
-		} else if (time.minute < currentDate.getMinutes()) {
-			const minutesAgo = (currentDate.getMinutes() - time.minute);
-			return `${minutesAgo} ${minutesAgo > 1? 'minutos': 'minuto'} atrás`;
-		}  else if (time.second < currentDate.getSeconds()) {
-			const secondsAgo = (currentDate.getSeconds() - time.second);
-			return `${secondsAgo} ${secondsAgo > 1? 'segundos': 'segundo'} atrás`;
+		} else if (diffDays >= 30) {
+			// Quando foi postado há meses.
+			const monthsAgo = Math.floor(diffDays / 30) || 1;
+			return `Há ${numbers[monthsAgo - 1]} ${monthsAgo > 1 ? 'meses': 'mes'}`;
+
+		} else if (diffDays >= 7) {
+			// Semanas
+			const weeksAgo = Math.floor(diffDays / 7) || 1;
+			const dictionaryWeek = ['uma', 'duas']
+			return `Há ${weeksAgo > 2 ? numbers[weeksAgo - 1]
+					: dictionaryWeek[weeksAgo - 1]} ${weeksAgo > 1 ? 'semanas': 'semana'}`;
+
+		} else if (diffDays >= 1) {
+			// Dias
+			return `Há ${numbers[diffDays - 1]} ${diffDays >= 2? 'dias': 'dia'}`;
+
+		} else if (diffHours >= 1) {
+			// Horas
+			return `${diffHours} ${diffHours > 1? 'horas': 'hora'} atrás`;
+
+		} else if (diffMinutes >= 1) {
+			// Minutos
+			return `${diffMinutes} ${diffMinutes > 1? 'minutos': 'minuto'} atrás`;
+
+		}  else if (diffSeconds >= 1) {
+			// Segundos
+			return `${diffSeconds} ${diffSeconds > 1? 'segundos': 'segundo'} atrás`;
+
 		} else {
 			return 'Agora mesmo';
 		}
